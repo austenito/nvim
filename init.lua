1124bb603411a7aa8ed2cd2aecb72453ab266527
poptarts <dev@poptarts.dev>
2026-05-01 23:06:10 -0400

    Add day and night themes

--- init.lua
+++ init.lua
@@ -13,4 +13,12 @@ require "lsp"
 -- [[ Setup lazy plugin manager ]]
 require "config.lazy"
 
-vim.cmd.colorscheme "kanagawa-paper-canvas"
+local hour = tonumber(os.date "%H")
+if hour >= 7 and hour < 18 then
+  vim.o.background = "light"
+  vim.cmd.colorscheme "tokyonight-day"
+else
+  -- Night theme
+  vim.o.background = "dark"
+  vim.cmd.colorscheme "tokyonight-moon"
+end
